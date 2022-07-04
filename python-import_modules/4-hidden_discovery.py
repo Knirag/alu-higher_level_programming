f3efe34fa6f0d123145eca9d265b05a9a8a46ab2
Knirag <k.niragire@alustudent.com>
2022-07-04 15:48:09 -0700

    Task 4

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import hidden_4
-    for x in dir(hidden_4):
-        if x[:2] != "__":
-            print("{:x}".format(x))
+    import hidden_4 as extra
+
+
+for x in dir(extra):
+    if x[0:2] != "__":
+        print("{:s}".format(x))
