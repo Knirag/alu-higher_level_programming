de8cbf4973a82b1b3ed9b3af2ec0ccfea897c534
Knirag <k.niragire@alustudent.com>
2022-07-22 10:52:58 -0700

    Task 1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-"""NO MODULES USED IN THIS CODE"""
+"""Define a class Square."""
 
 
 class Square:
-    """The Class defines the sizes of a square
-    and show any errors and exceptions"""
+    """Represent a square."""
 
-    def __init__(self, size=0):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        self.square_size = size
+    def __init__(self, size):
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
