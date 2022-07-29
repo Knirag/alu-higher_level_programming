b78347f2c0d1e3512096ad8c040f5ae9817744d1
Knirag <k.niragire@alustudent.com>
2022-07-29 12:55:14 -0700

    Task 11

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,32 +1,30 @@
 #!/usr/bin/python3
-""" Module that defines the class Student
-"""
+""" class student """
 
 
 class Student:
-    """ Class to create student instances """
-
+    """ define function"""
     def __init__(self, first_name, last_name, age):
-        """ Special method to initialize """
+        """
+        :param self:
+        :param first_name:
+        :param last_name:
+        :param age:
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """ Method that returns directory description """
-        obj = self.__dict__.copy()
-        if type(attrs) is list:
+        if attrs is None:
+            return self.__dict__
 
-            for item in attrs:
-                if type(item) is not str:
-                    return obj
+        return ({key: value for key, value in self.__dict__.items()
+                 if key in attrs})
 
-            d_list = {}
-
-            for iatr in range(len(attrs)):
-                for satr in obj:
-                    if attrs[iatr] == satr:
-                        d_list[satr] = obj[satr]
-            return d_list
-
-        return obj
+    def reload_from_json(self, json):
+        """
+        replace all attr of the
+        student instance
+        """
+        self.__dict__.update(json)
