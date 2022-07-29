a0d141675025d39f0bbb272fe481477b7f524e09
Knirag <k.niragire@alustudent.com>
2022-07-29 12:53:27 -0700

    Task 11

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,54 +1,32 @@
 #!/usr/bin/python3
-"""
-A Student class that defines a Studen (module)
+""" Module that defines the class Student
 """
 
 
-<<<<<<< HEAD
 class Student:
-    """ 
-    this is a student class 
-    """
+    """ Class to create student instances """
 
     def __init__(self, first_name, last_name, age):
-        """
-        just doing init for variables
-=======
-class Student():
-    """
-        A Student class that defines a Student
-    """
-    def __init__(self, first_name, last_name, age):
-        """
-            INIT
->>>>>>> 6427b0b5935bbdf2227a51d2669076a7153f394f
-        """
+        """ Special method to initialize """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """
-<<<<<<< HEAD
-        method converts to json 
-=======
-            retrieves a dictionary representation
-            of a Student instance
->>>>>>> 6427b0b5935bbdf2227a51d2669076a7153f394f
-        """
-        if type(attrs) is list and all([type(x) == str for x in attrs]):
-            return {k: v for k, v in self.__dict__.items() if k in attrs}
-        return(self.__dict__)
+        """ Method that returns directory description """
+        obj = self.__dict__.copy()
+        if type(attrs) is list:
 
-    def reload_from_json(self, json):
-        """
-<<<<<<< HEAD
-        return json 
-        """
-        for i,j in json.items():
-            self.__dict__(i) = j
-=======
-        """
-        for i, j in json.items():
-            self.__dict__[i] = j
->>>>>>> 6427b0b5935bbdf2227a51d2669076a7153f394f
+            for item in attrs:
+                if type(item) is not str:
+                    return obj
+
+            d_list = {}
+
+            for iatr in range(len(attrs)):
+                for satr in obj:
+                    if attrs[iatr] == satr:
+                        d_list[satr] = obj[satr]
+            return d_list
+
+        return obj
