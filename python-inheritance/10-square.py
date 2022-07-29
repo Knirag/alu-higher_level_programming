6f40b4431fac898b56df34bb4d98b7fc148b5f6d
Knirag <k.niragire@alustudent.com>
2022-07-29 12:16:53 -0700

    Task 10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,41 +1,16 @@
 #!/usr/bin/python3
-"""
-class Rectangle inherits from class BaseGeometry
-after importing the module
-"""
-
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""Defines a Rectangle subclass Square."""
+Rectangle = __import__('9-rectangle').Rectangle
 
 
-class Rectangle(BaseGeometry):
-    """ class rectangle that inherits from class BaseGeometry"""
+class Square(Rectangle):
+    """Represent a square."""
 
-    def __init__(self, width, height):
+    def __init__(self, size):
+        """Initialize a new square.
+        Args:
+            size (int): The size of the new square.
         """
-        initialises rectangle
-        :param width:
-        :param height:
-        """
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """
-        Returns the area of the `Rectangle` instance.
-        """
-        return (self.__width * self.__height)
-
-    def __str__(self):
-        """
-        string representation.
-        """
-        return "[Rectangle] " + str(self.__width) + "/" + str(self.__height)
-
-
-if __name__ == '__main__':
-    r = Rectangle(3, 5)
-
-    print(r)
-    print(r.area())
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
