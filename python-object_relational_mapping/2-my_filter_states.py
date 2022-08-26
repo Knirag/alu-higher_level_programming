ddefdda17534cf5e229bfd19a29f67a4bb09802d
Knirag <k.niragire@alustudent.com>
2022-08-26 08:41:41 -0700

    Task 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -8,12 +8,14 @@ if __name__ == "__main__":
     # port and host are default local and 3306
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("""SELECT * FROM states ORDER BY states.id ASC""")
+    # use string formating to be specific
+    cur.execute("SELECT * FROM states WHERE states.name = '{}'\
+    ORDER BY states.id ASC".format(argv[4]))
     result = cur.fetchall()
-    # display elements with N
-    # only by taking comparing their first letter in tuple
+    # check if second argument of tuple
+    # is same as the passed argument
     for i in result:
-        if i[1][0] == 'N':
+        if i[1] == argv[4]:
             print(i)
     # close cursor and db
     cur.close()
